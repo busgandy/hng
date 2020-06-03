4e8d86789be0abe49faa2131f14fc1303a69f626
Busayo Olushola <gideonbusayo@gmail.com>
2020-06-03 12:32:27 +0100

    Added Busayo-Olushola.py

--- Busayo-Olushola.py
+++ Busayo-Olushola.py
@@ -6,8 +6,10 @@ def print_details():
     HNGi7_ID = ("HNG-02411")
     language = ("python")
     task = ("stage 2 task")
-    print (f"Hello World, this is {full_name} with HNGi7_ID {HNGi7_ID} using {language} for {task}")
+    e_mail = ("gideonbusayo@gmail.com")
+    print (f"Hello World, this is {full_name} with HNGi7 ID {HNGi7_ID} and {e_mail} using {language} for {task}")
 
 
 if __name__ == "__main__":
     main()
+
